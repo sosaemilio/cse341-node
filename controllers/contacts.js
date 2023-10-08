cf3e67433848815f89020fd0855aac623767b73d
Emilio Sosa <emiliossosa85@gmail.com>
2023-10-07 23:42:44 -0400

    Create API docs

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,5 +1,5 @@
-const mongodb = require('../db/connect');
 const { ObjectId } = require('mongodb');
+const mongodb = require('../db/connect');
 
 const getContacts = async function (req, res) {
   const contacts = await mongodb.getDb().db('byui').collection('contacts').find({});
@@ -11,7 +11,7 @@ const getContacts = async function (req, res) {
 
 const getContactById = async function (req, res) {
   const contactId = new ObjectId(req.params.id);
-  const contact = await mongodb.getDb().db('byui').collection('contacts').find({ _id: contactId});
+  const contact = await mongodb.getDb().db('byui').collection('contacts').find({ _id: contactId });
   contact.toArray().then((list) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(list);
@@ -19,26 +19,50 @@ const getContactById = async function (req, res) {
 };
 
 const addContact = async function (req, res) {
+  /*
+  #swagger.consumes['body'] = {
+    in: 'body',
+    description: 'Some description...',
+    schema: {
+        $name: 'John Doe',
+        $age: 29,
+        about: ''
+    }
+  } 
+  */
+
   const newContact = req.body;
-  const result = await mongodb.getDb().db('byui').collection('contacts').insertOne(newContact, function (err) {
-    if (err) res.status(500).send(err);
-  });
+  const result = await mongodb
+    .getDb()
+    .db('byui')
+    .collection('contacts')
+    .insertOne(newContact, (err) => {
+      if (err) res.status(500).send(err);
+    });
 
   if (result) res.status(201).json(result);
-}
+};
 
 const updateContact = async function (req, res) {
   const contactId = new ObjectId(req.params.id);
   const data = req.body;
-  const result = await mongodb.getDb().db('byui').collection('contacts').updateOne({_id : contactId}, {$set : data});
+  const result = await mongodb
+    .getDb()
+    .db('byui')
+    .collection('contacts')
+    .updateOne({ _id: contactId }, { $set: data });
   if (result) res.status(204).json(result);
-}
+};
 
 const deleteContact = async function (req, res) {
   const contactId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db('byui').collection('contacts').deleteOne({_id : contactId});
+  const result = await mongodb
+    .getDb()
+    .db('byui')
+    .collection('contacts')
+    .deleteOne({ _id: contactId });
   if (result) res.status(200).json(result);
-}
+};
 
 module.exports = {
   getContacts,
